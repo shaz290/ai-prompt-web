6bae81023ff41c2d934bcb022f7c0839e72e31e2
shaz.ahmed <shaz.ahmed@KAN-QALAP101>
2026-01-02 18:04:09 +0530

    image change

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -88,20 +88,20 @@ export const Hero = () => {
                     </div>
                     {/*Right Column -Profile Image*/}
 
-                    {/* <div className="relative animate-fade-in animation-delay-300"> */}
+                    <div className="relative animate-fade-in animation-delay-300">
                         {/*Profile Image*/}
-                        {/* <div className="relative max-w-md mx-auto"> */}
+                        <div className="relative max-w-md mx-auto">
 
                             {/*glowing*/}
-                            {/* <div className="absolute inset-0 rounded-3xl bg-gradient-to-br from-primary/30 
+                            <div className="absolute inset-0 rounded-3xl bg-gradient-to-br from-primary/30 
                             via-transparent to-primary/10 blur-2xl animate-pulse"/>
                             <div className="relative glass rounded-3xl p-2 glow-border">
-                                <img src='/profile-photo.jpg' alt="The Dev master"
+                                <img src='/profile-photo.jpeg' alt="The Dev master"
                                     className="w-full aspect-[4/5] object-cover rounded-2xl" />
 
-                            </div> */}
-                        {/* </div> */}
-                    {/* </div> */}
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
